8b421fdeac4a7b8c14e783c1dd5a370dea9e2244
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-05-17 11:01:05 +0500

    type orm(for postgres) base config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,24 @@ import { AppService } from './app.service';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { GraphQLModule } from '@nestjs/graphql';
 import { UserModule } from 'src/features/users/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'it_link',
+      password: '12345',
+      database: 'it_link',
+      entities: [],
+      autoLoadEntities: true,
+      synchronize: true,
     }),
     UserModule,
   ],
