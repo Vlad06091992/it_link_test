c03382b529b066a6b3c9d8d077761354b6a5171e
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-05-18 13:12:10 +0500

    lint fix

--- src/features/colors/entity/colors.entity.ts
+++ src/features/colors/entity/colors.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'v_color' })
 export class ColorsEntity {
